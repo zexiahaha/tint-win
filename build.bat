3137eca2ea0e67bf3e91288ba998640a8ac55a2c
hahazexia <hahazexia@outlook.com>
2026-04-23 17:40:46 +0800

    create window

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 mkdir .\build
 pushd .\build
-cl -Zi ..\tint.c
+cl -Zi ..\tint.c User32.lib
 popd
--- tint.c
+++ tint.c
@@ -3,20 +3,65 @@
 
 LRESULT Win32MainWindowCallback(
                                 HWND Window,
-                                UINT MEssage,
+                                UINT Message,
                                 WPARAM WParam,
                                 LPARAM LParam
-                                ) {
-                                    LRESULT Result = 0;
-                                    return Result;
-                                }
+                                )
+{
+    LRESULT Result = 0;
+    switch (Message)
+    {
+        case WM_CLOSE:
+        {
+            return 0;
+            break;
+        }
+        case WM_SIZE:
+        {
+            return 0;
+            break;
+        }
+        case WM_DESTROY:
+        {
+            return 0;
+            break;
+        }
+        default: {
+            return DefWindowProc(Window, Message, WParam, LParam);
+        }
+    }
+    return Result;
+}
 
 int WinMain(HINSTANCE MainHInstance, HINSTANCE MainHInstancePrev, PSTR cmdline, int cmdshow)
 {
-    WNDCLASS WndClass = {};
-    WndClass.lpfnWndProc = Win32MainWindowCallback;
-    WndClass.hInstance = MainHInstance;
-    WndClass.lpszClassName = "TintWindowClass";
+    WNDCLASS WindowClass = {};
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
+    WindowClass.hInstance = MainHInstance;
+    WindowClass.lpszClassName = "TintWindowClass";
+
+    if (!RegisterClass(&WindowClass))
+    {
+        HWND HWindow = CreateWindowEx(
+                                      0,
+                                      "TintWindowClass",
+                                      "tint-win",
+                                      0,
+                                      0,
+                                      0,
+                                      50,
+                                      50,
+                                      NULL,
+                                      NULL,
+                                      MainHInstance,
+                                      NULL
+                                      );
+
+        if (HWindow != NULL)
+        {
+            
+        }
+    }
     
     return 0;
 }
